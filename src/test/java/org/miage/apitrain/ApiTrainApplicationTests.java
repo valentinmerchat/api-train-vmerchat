a34e8f31930f61382eebbbe5fc52ef76d7091d56
valentinmerchat <valentin.merchat1@outlook.fr>
2022-02-28 11:52:17 +0100

    Commit install projet

---
+++ src/test/java/org/miage/apitrain/ApiTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miage.apitrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
